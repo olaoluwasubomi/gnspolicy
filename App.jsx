83e830830ea256b3ab4cc2d873e890b91d044c5c
olaoluwasubomi <73838337+olaoluwasubomi@users.noreply.github.com>
2024-03-25 12:13:01 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Privacy from "./Privacy";
+import Nav from "./Nav";
+const App = () =>{
+  return(
+    <div>
+       <Nav />
+      <Privacy />
+    </div>
+  )
+}
+export default App;
